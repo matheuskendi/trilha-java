188f73000efd384a98d91e02de3ad2133beb2907
kendi <matheus.mks23@gmail.com>
2025-03-13 09:05:24 -0300

    Tipos de Operadores

---
+++ tipos-variaveis/src/TiposOperadores.java
@@ -0,0 +1,47 @@
+public class TiposOperadores {
+   // public static void main(String[] args){
+        
+    //    String nome1 = "Matheus";
+    //    String nome2 = new String("Matheus");
+//
+    //    System.out.println(nome1.equals(nome2));
+//
+//
+    //    int num1 = 5;
+    //    int num2 = 6;
+//
+//
+    //    boolean simNao = num1 == num2;
+//
+    //    System.out.println("numUm e igual a numDois ? " + simNao);
+    //
+    //    simNao = num1 != num2;
+//
+    //    System.out.println("numUm e diferente a numDois ? " + simNao);
+    //
+    //    simNao = num1 >= num2;
+//
+    //    System.out.println("numUm e maior a numDois ? " + simNao);
+    //
+    //    simNao = num1 <= num2;
+//
+    //    System.out.println("numUm e menor a numDois ? " + simNao);
+    //
+    //}
+    public static void main(String[] args){
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if(condicao1 && (8 > 5)){
+            System.out.println("As duas contições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições são verdadeira");
+        }
+
+        System.out.println("FIM...");
+    }
+
+}
