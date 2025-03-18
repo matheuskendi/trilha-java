0803fe6a23fb8bdf8772fff4921608924d4dadb8
kendi <matheus.mks23@gmail.com>
2025-03-18 08:27:48 -0300

    For

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int carnerinhos = 1; carnerinhos <= 20; carnerinhos ++){
+            System.out.println("contando carnerinhos: " + carnerinhos);
+        }
+
+        System.out.println("Matheus dormiu!");
+    }
+}
---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"Matheus" , "Fabio", "Juliana", "Abençoado"};
+
+        for (int x = 0 ; x < alunos.length; x++){
+            System.out.println("Aluno no indice x=" + x + " é o : " + alunos [x]);
+
+        }
+    }
+    
+}
